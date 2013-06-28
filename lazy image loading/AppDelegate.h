ddc86346f740b7b09658a6007c7a3cbd05c895f0
k comp <kcomp@ks-Mac-mini.local>
2013-06-28 11:36:16 +0530

    Initial Commit

---
+++ lazy image loading/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  lazy image loading
+//
+//  Created by k comp on 28/06/13.
+//  Copyright (c) 2013 k comp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ lazy image loading/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  lazy image loading
+//
+//  Created by k comp on 28/06/13.
+//  Copyright (c) 2013 k comp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
